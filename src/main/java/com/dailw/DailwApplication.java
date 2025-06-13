9a7013886e4b8041c9e1dbbf8da08df5b10b9240
dailiwei <3031683178@qq.com>
2025-06-13 17:57:51 +0800

    初始化

---
+++ src/main/java/com/dailw/DailwApplication.java
@@ -0,0 +1,13 @@
+package com.dailw;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DailwApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DailwApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/dailw/DailwApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dailw;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DailwApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
